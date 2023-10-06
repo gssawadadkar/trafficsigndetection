b8b20448d2c492c849f6e8c8559ebf895f153f8e
gajanan sawadadkar <gssawadadkar@gmail.com>
2023-10-06 13:31:48 +0530

    add code for main_util and exception handling

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 from trafficsigndetection.logger import logging
-
-logging.info("welcome to my custom log")
+from trafficsigndetection.exception import AppException
+import sys
+# Example usage
+try:
+    # Some code that might raise an exception
+    x = 1 / "s"
+except ZeroDivisionError as e:
+    app_exception = AppException("Cannot divide by zero")
+    error_message = app_exception.error_message_detail()
+    print(error_message)
